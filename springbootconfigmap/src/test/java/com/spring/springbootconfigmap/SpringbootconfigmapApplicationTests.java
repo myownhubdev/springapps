6cb6096faf462e7ccedd38e54407d79e9e316a2d
myownhubdev <42083208+myownhubdev@users.noreply.github.com>
2018-08-03 23:51:55 -0500

    Add files via upload
    properties externalized

---
+++ springbootconfigmap/src/test/java/com/spring/springbootconfigmap/SpringbootconfigmapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.springbootconfigmap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootconfigmapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
